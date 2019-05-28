99fc514440af5cdd50183d249ac69baa12b4aac3
xuanzai <43233731+MikuBlog@users.noreply.github.com>
2019-05-28 09:14:27 +0800

    保留对应倍数索引元素

--- every_nth.js
+++ every_nth.js
@@ -1,7 +1,7 @@
 	// 保留对应倍数索引元素
 	function everyNth(arr, nth) {
 		return arr.filter((val, ind) => ind % nth === nth - 1)
-	}
+	} 
 	
 	everyNth([1, 2, 3, 4, 5, 6], 3)// [3, 6]
 	everyNth([1, 2, 3, 4, 5, 6], 2)// [2, 4, 6]
