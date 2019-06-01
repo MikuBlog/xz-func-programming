6316ee7fc68f553fb0bf3ed604e8843236efd605
MikuBlog <786601672@qq.com>
2019-06-01 11:25:18 +0800

    简单模拟实现数组的reduce方法

---
+++ array_reduce.js
@@ -0,0 +1,13 @@
+	// 简单模拟array的reduce方法
+	function arrayReduce(arr, fn, initialVal) {
+		let val = "", i
+		initialVal ? (val = initialVal, i = 0) : (val = arr[0], i = 1)
+		while(arr[i]) {
+			arr[i] && (val = fn(val, arr[i]))
+			i ++
+		}
+		return val
+	}
+	arrayReduce([1, 2, 3], (acc, next) => acc + next)// 6
+	arrayReduce([1, 2, 3], (acc, next) => acc + next, 5)// 11
+	
