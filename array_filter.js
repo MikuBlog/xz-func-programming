8deb9be6229428b9c20358e4be8ae6f4232a4d32
MikuBlog <786601672@qq.com>
2019-06-01 16:36:11 +0800

    简单模拟实现array的filter方法

---
+++ array_filter.js
@@ -0,0 +1,10 @@
+	// 模拟实现filter函数
+	function arrayFilter(arr, fn) {
+		let newArray = []
+		for(let i = 0, len = arr.length; i < len; i ++) {
+			fn(arr[i]) && newArray.push(arr[i])
+		}
+		return newArray
+	}
+	
+	arrayFilter([1, 2, 3, 4], val => val >= 3)// [3, 4]
