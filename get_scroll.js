03f952649214990d4396b58a7b289d9c5d60242c
xuanzai <43233731+MikuBlog@users.noreply.github.com>
2019-06-03 16:39:29 +0800

    获取滚动距离

--- get_scroll.js
+++ get_scroll.js
@@ -2,6 +2,6 @@
 	function getScroll() {
 		return {
 			left: window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft || 0,
-			right: window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0, 
+			top: window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0, 
 		}
 	}
