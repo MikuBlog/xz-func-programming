186034b427b406cf29220cdabd09e66ba2075a58
xuanzai <43233731+MikuBlog@users.noreply.github.com>
2019-06-30 10:05:39 +0800

    对象序列化

---
+++ obj_to_string.js
@@ -0,0 +1,13 @@
+    // 对象序列化
+		function objToString(obj) {
+			let str = ""
+			for(let key in obj) {
+				str += `${key}=${obj[key]}&`
+			}
+			return str.replace(/&$/, '')
+		}
+		
+		objToString({
+			name: "小狗",
+			favor: "跑"
+		}) // name=小狗&favor=跑
