654de6b515fd4773c3702bc7ae39c4726155d087
MikuBlog <786601672@qq.com>
2019-08-02 09:18:05 +0800

    类型检测

---
+++ type.js
@@ -0,0 +1,27 @@
+	function type(val) {
+		return Object
+				.prototype
+				.toString
+				.call(val)
+				.split(' ')[1]
+				.split(']')[0]
+				.toLowerCase()
+	}
+	
+	[
+		"Number",
+		"Boolean",
+		"String",
+		"Object",
+		"Null",
+		"Undefined",
+		"Symbol",
+		"Array"
+	].forEach(value => {
+		type[`is${value}`] = (val) => {
+			return type(val) === value.toLowerCase()
+		}
+	})
+	
+	type.isObject({}) // true
+	type.isNumber(1) // true
