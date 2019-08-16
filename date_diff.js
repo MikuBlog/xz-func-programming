f704eb4a7b3c8bc6b290d566821d630f72b21e3c
mikublog <786601672@qq.com>
2019-08-16 10:06:41 +0800

    完善时间差函数'/

--- date_diff.js
+++ date_diff.js
@@ -1,9 +1,18 @@
 	// 日期天数差
-	function date_diff(sDate1,  sDate2){   
-	    const iDays = sDate1 < sDate2 ? (new Date(sDate2) - new Date(sDate1)) : (new Date(sDate1) - new Date(sDate2))
+	function date_diff(sDate1,  sDate2, isNegative = false){   
+		let iDays = ""
+		isNegative
+		? iDays = new Date(sDate1) - new Date(sDate2)
+		: iDays = sDate1 < sDate2
+		? (new Date(sDate2) - new Date(sDate1))
+		: (new Date(sDate1) - new Date(sDate2))
 	    return iDays / 1000 / 60 / 60 / 24 
 	}
 	
-	date_diff("2019-01-01", "2018-12-31")// 1
+	date_diff("2019-01-01", "2018-12-31") // 1
+	
+	date_diff("2018-12-31", "2019-01-01") // 1
+	
+	date_diff("2018-12-31", "2019-01-01", true) // -1
 	
 	
