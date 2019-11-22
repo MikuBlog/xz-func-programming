0fa2230b32a6681ad3d57a824f08ebb81b194c8d
mikublog <786601672@qq.com>
2019-11-22 16:08:19 +0800

    获取url中的参数

--- url_query.js
+++ url_query.js
@@ -1,14 +1,20 @@
-	// 获取url后的参数
-	function urlQuery() {
-		let query = {}
-		if(window.location.search) {
-			let list = window.location.search.replace("?", "")
-			list.split("&").forEach(val => {
-				const 
-					key = val.split("=")[0],
-					value = val.split("=")[1]
-				query[key] = value
-			})
-		}
-		return query
+	// 获取url后的参数或传入的url后的参数
+	function urlQuery(url = "") {
+	  let
+	    query = {},
+	    param = ""
+	  if (url.split("?")[1]) {
+	    param = url.split("?")[1]
+	  } else if (window.location.search) {
+	    param = window.location.search.replace("?", "")
+	  } else {
+	    return query
+	  }
+	  param.split("&").forEach(val => {
+	    const
+	      key = val.split("=")[0],
+	      value = val.split("=")[1]
+	    query[key] = value
+	  })
+	  return query
 	}
