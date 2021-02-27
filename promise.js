91e270454824380d631326a2d2e3fd29e6178946
mikublog <786601672@qq.com>
2021-02-27 13:23:21 +0800

    更新代码

--- promise.js
+++ promise.js
@@ -1,143 +1,147 @@
-	// 实现Promise构造函数
-	function Promise(executor) {
-		const self = this
-		// 初始状态为准备(就绪)状态
-		self.status = 'pending'
-		// 初始值为空
-		self.value = undefined
-		// 订阅事件数组
-		self.onResolvedCallBacks = []
-		self.onRejectedCallBacks = []
-		// Promise的resolve,旨在将Promise实例的状态转化为fulfilled,并且发布所有订阅的完成事件
-		function resolve(value) {
-			if(value instanceof Promise) {
+function Promise(fn) {
+	let self = this
+	this.status = 'pending'
+	this.value = undefined
+	this.reason = undefined
+	this.onResolvedCallbacks = []
+	this.onRejectedCallbacks = []
+
+	function resolve(value) {
+		setTimeout(_ => {
+			if (value instanceof Promise) {
 				return value.then(resolve, reject)
 			}
-			// 如果状态为'pending'(就绪)状态,改为fulfilled(完成)状态
-			if(self.status === "pending") {
+			if (self.status === 'pending') {
 				self.status = 'fulfilled'
 				self.value = value
-				// 发布所有已经订阅的完成事件
-				self.onResolvedCallBacks.forEach(item => item(self.value))
+				self.onResolvedCallbacks.forEach(val => val())
 			}
-			return self
-		}
-		// Promise的reject,旨在将Promise实例的状态转化为rejected,并且发布所有订阅的拒绝事件
-		function reject(reason) {
-			if(self.status === 'pending') {
+		})
+	}
+
+	function reject(reason) {
+		setTimeout(_ => {
+			if (self.status === 'pending') {
 				self.status = 'rejected'
-				self.value = reason
-				// 发布所有已经订阅的拒绝事件
-				self.onRejectedCallBacks.forEach(item => item(self.value))
+				self.reason = reason
+				self.onRejectedCallbacks.forEach(val => val())
 			}
-		}
-		// 立刻执行函数
+		})
+	}
+	try {
+		fn(resolve, reject)
+	} catch (e) {
+		reject(e)
+	}
+}
+// Promise中转站
+function centorPromise(promise, val, resolve, reject) {
+	if (promise.status !== 'rejected') {
 		try {
-			executor(resolve, reject)
-		}catch(e) {
+			if (val instanceof Promise) {
+				val.then(resolve, reject)
+			} else {
+				resolve(val)
+			}
+		} catch (e) {
 			reject(e)
 		}
+	} else {
+		reject(promise.reason)
 	}
-	
-	Promise.prototype.then = function(onFulfilled, onRejected) {
-		// 当没有函数传进来的时候,添加默认函数
-		onFulfilled = typeof onFulfilled === 'function' ? onFulfilled: function(value) {
-			return value
-		},
-		onRejected = typeof onRejected === 'function' ? onRejected: function(err) {
-			throw err
-		},
-		// 将this上下文保存起来
-		const self = this
-		let promise
-		if(self.status === 'fulfilled') {
-			// 当状态为fulfilled时执行传过来的第一个函数
-			promise = new Promise((resolve, reject) => {
-				setTimeout(function() {
-					try {
-						onFulfilled(self.value)
-					}catch(e) {
-						reject(e)
-					}
-				})
+}
+// Promise.then实现
+Promise.prototype.then = function(onFulfilledFn, onRejectedFn) {
+	let
+		self = this,
+		promise = null
+	onFulfilledFn = onFulfilledFn && typeof onFulfilledFn === 'function' ?
+		onFulfilledFn :
+		val => {
+			return val
+		}
+	onRejectedFn = onRejectedFn && typeof onRejectedFn === 'function' ?
+		onRejectedFn :
+		val => {
+			return val
+		}
+	return promise = new Promise((resolve, reject) => {
+		if (self.status === 'pending') {
+			self.onResolvedCallbacks.push(() => {
+				try {
+					let result = onFulfilledFn(self.value)
+					centorPromise(self, result, resolve, reject)
+				} catch (e) {
+					self.reason = e
+					reject(e)
+				}
+			})
+			self.onRejectedCallbacks.push(() => {
+				try {
+					let result = onRejectedFn(self.reason)
+					centorPromise(self, result, resolve, reject)
+				} catch (e) {
+					self.reason = e
+					reject(e)
+				}
 			})
 		}
-		if(self.status === 'rejected') {
-			// 当状态为rejected时,执行传过来的第二个函数
-			promise = new Promise((resolve, reject) => {
-				setTimeout(function() {
-					try {
-						onRejected(self.value)
-					}catch(e) {
-						reject(e)
-					}
-				})
-			})
-		}
-		if(self.status === 'pending') {
-			// 当状态为pending时,将函数订阅
-			promise = new Promise((resolve, reject) => {
-				self.onResolvedCallBacks.push(function() {
-					try {
-						onFulfilled(self.value)
-					}catch(e) {
-						reject(e)
-					}
-				})
-				self.onResolvedCallBacks.push(function() {
-					try {
-						onFulfilled(self.value)
-					}catch(e) {
-						reject(e)
-					}
-				})
-			})
-		}
-		return promise
-	}
-	
-	// 传入第二个参数(实际上就是调用then方法)
-	Promise.prototype.catch = function(onRejected) {
-		return this.then(null, onRejected)
-	}
-	
-	Promise.all = function(promises) {
-		return new Promise((resolve, reject) => {
-			// 保存成功执行的次数
-			let count = 0
-			// 保存成功执行后返回的参数
-			let result = []
-			for(let i = 0; i < promises.length; i ++) {
-				promises[i].then((data) => {
-					result[i] = data
-					// 如果都成功执行,将所有参数往then传
-					if(++ count === promises.length) {
-						resolve(result)
-					}
-				}, (err) => {
-					reject(err)
-				})
+		if (self.status === 'fulfilled') {
+			try {
+				let result = onFulfilledFn(self.value)
+				centorPromise(self, result, resolve, reject)
+			} catch (e) {
+				self.reason = e
+				reject(e)
 			}
-		})
-	}
-	
-	Promise.race = function(promises) {
-		return new Promise((resolve, reject) => {
-			// 如果有一个已经完成的话,就立刻执行resolve,将外层Promise状态进行转换
-			for(let i = 0; i < promises.length; i ++) {
-				promises[i].then(resolve, reject)
+		}
+		if (self.status === 'rejected') {
+			try {
+				let result = onRejectedFn(self.reason)
+				centorPromise(self, result, resolve, reject)
+			} catch (e) {
+				self.reason = e
+				reject(e)
 			}
-		})
-	}
-	
-	Promise.resolve = function(value) {
-		return new Promise((resolve, reject) => {
-			resolve(value)
-		})
-	}
-	
-	Promise.reject = function(reason) {
-		return new Promise((resolve, reject) => {
-			reject(reason)
-		})
-	} 
+		}
+	})
+	return promise
+}
+Promise.catch = function(onRejected) {
+	return this.then(null, onRejected)
+}
+Promise.all = function(promises) {
+	return new Promise((resolve, reject) => {
+		let
+			count = 0,
+			result = []
+		for (let i = 0, len = promises.length; i < len; i++) {
+			promises[i].then(result => {
+				result.push(result)
+				if (++count === promises.length) {
+					resolve(result)
+				}
+			}).catch(e => {
+				reject(e)
+			})
+		}
+	})
+}
+// 只要有一个promise状态改变了就立刻resolve或reject对应的那个promise
+Promise.race = function(promises) {
+	return new Promise((resolve, reject) => {
+		for (let i = 0, len = promises.length; i < len; i++) {
+			promises[i].then(resolve, reject)
+		}
+	})
+}
+Promise.resolve = function(value) {
+	return new Promise((resolve, reject) => {
+		resolve(value)
+	})
+}
+Promise.reject = function(reason) {
+	return new Promise((resolve, reject) => {
+		reject(reason)
+	})
+}
